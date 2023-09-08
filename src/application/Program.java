23fd4e522ab6457cd281aa5405e46714a5efe554
Matheus Eler <matheus.eler@outlook.com>
2023-09-08 20:13:11 -0300

    find by add implentacion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,27 @@
 package application;
 
-import java.util.Date;
+import java.util.Scanner;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
+		System.out.print("Digite um ID para pesquisar um vendedor: ");
+		Scanner in = new Scanner(System.in);
+		Integer n = in.nextInt();
+		
+		Seller seller = sellerDao.findById(n);
+		
+		System.out.println("Segue abaixo os dados do vendedor desejado:\n\n");
+		System.out.println("------------------------");
+		System.out.println();
+		System.out.println(seller);
 	}
 
 }
